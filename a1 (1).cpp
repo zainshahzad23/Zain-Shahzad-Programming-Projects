00187c4826e5e97c71a784294041fd07f53a47f5
zainshahzad23 <84042671+zainshahzad23@users.noreply.github.com>
2021-05-11 16:13:50 -0400

    Add files via upload

---
+++ a1 (1).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+const int n = 2;
+int main()
+{
+   int numbers [n];
+   numbers[0] = 62;
+   numbers[1] = 99;
+   cout<<"total"<<(numbers[0]+numbers[1]);
+   return 0;
+}
+   
