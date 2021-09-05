e165639b6af91ba229f9db70ad028278f3adec69
zainshahzad23 <84042671+zainshahzad23@users.noreply.github.com>
2021-09-05 11:44:14 -0400

    Add files via upload

---
+++ Chess.cpp
@@ -0,0 +1,75 @@
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+//this project is in progrees as of 9/05/2021
+{
+    
+    char pieceone,piecetwo,piecethree,piecefour,piecefive,piecesix;
+    pieceone='P';
+    piecetwo='B';
+    piecethree='k';
+    piecefour='R';
+    piecefive='Q';
+    piecesix='K';
+    cout << " A B C D E F G H \n\n";
+    cout << " P B KT R Q K\n\n";
+}
+    {
+        char row;
+        for (char row = 8, row = 0; row --;), /endl;
+        if (row %2 == 0)
+            printline(row, blacksquare, whitesquare);
+        else
+            printline(row, whitesquare, blacksquare);
+        break;
+//might have to combine the blacksquare/whitesquare with the grid and possibly repeat line 20 for the grid
+            {
+                char grid[8][8] =
+                    {'A8','B8','C8','D8','E8','F8','G8','H8'}
+                    {'A7','B7','C7','D7','E7','F7','G7','H7'}
+                    {'A6','B6','C6','D6','E6','F6','G6','H6'}
+                    {'A5','B5','C5','D5','E5','F5','G5','H5'}
+                    {'A4','B4','C4','D4','E4','F4','G4','H4'}
+                    {'A3','B3','C3','D3','E3','F3','G3','H3'}
+                    {'A2','B2','C2','D2','E2','F2','G2','H2'}
+                    {'A1','B1','C1','D1','E1','F1','G1','H1'}}
+        //figure out what to do with the char section??
+                char section[1][8] = {'A','B','C','D','E','F','G','H'};
+                break
+        for grid
+            {
+                p = 'A2, B2, C2, D2, E2, F2, G2, H2';
+                B = 'B2,C3,D4,E5,F6,G7';
+                k ='B1,G1';
+                R = 'A1,H1';
+                Q = 'E1';
+                K = 'D1';
+                int piece = 6;
+                cout<<"what piece would you like to move?";
+                switch (piece)
+                    case 1;
+                    cout << "P";
+                      break;
+                    case 2:
+                      cout << "B";
+                      break;
+                    case 3:
+                      cout << "k";
+                      break;
+                    case 4:
+                      cout << "R";
+                      break;
+                    case 5:
+                      cout << "Q";
+                      break;
+                    case 6:
+                      cout << "K";
+                      break;
+                cin>>selectedpiece;
+                    if p
+                    {
+                        getgrid'(A2, B2, C2, D2, E2, F2, G2, H2)+y';
+                    }
+            }
